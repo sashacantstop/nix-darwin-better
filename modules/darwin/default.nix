b916d3b1cebe851e5e6e81b3b287f1dace8ebf6a
sfaye <sasha.faye175@gmail.com>
2026-08-03 02:00:28 -0400

    Update default.nix
    added basic settings

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,1 +1,13 @@
 # modules/darwin/default.nix
+{...}:
+{
+	nixpkgs.config.allowunfree = true;
+	nixpkgs.config.allowUnsupported = true;
+
+	nix.settings = {
+		experimental-features = [ "nix-command" "flakes" ];
+		auto-optimise-store = true;
+		warn-dirty = false;
+		sandbox = true;
+	}
+}
